948a3ce8cc0e01c636050203c8b6c467dc33f29d
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-07 16:52:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
   array.collect do |word| 
-    #{word}.reverse
+    word.reverse
   end
-  return array
 #  result = array.join(" ")
  # return result
 end
